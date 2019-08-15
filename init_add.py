56bb8113b93a1a73411bf2437c9bf60dbfb39f64
coolvasek <49400095+coolvasek@users.noreply.github.com>
2019-08-15 10:00:43 +0300

    Update init_add.py

--- init_add.py
+++ init_add.py
@@ -6,24 +6,25 @@ Base = declarative_base()
 from sqlalchemy import Column, Integer, String
 
 class Shop(Base):
-    __tablename__ = 'shops'
+    __tablename__ = 'shops2'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     tel = Column(Integer)
     address = Column(String)
     type = Column(String)
+    square = Column(Integer)
+    square_sell = Column(Integer)
+    floor = Column(Integer)
+    count_room = Column(Integer)
+    rent = Column(Integer)
+    comment = Column(String)
 
     chat_id = Column(Integer)
-    message_id = Column(Integer)
 
-    def _repr_(self):
-        return "<Shop(name='%s', tel='%s', address='%s')>" % (self.name, self.tel, self.address)
 
 
 def init():
     engine = create_engine('sqlite:///base.sqlite', echo=True)
-
-
 
     Base.metadata.create_all(engine)
 
@@ -33,6 +34,6 @@ def init():
     session = Session()
     return session
 
-def add(shop, session):
+def add(shop, session):  #запись в базу данных
     session.add(shop)
     session.commit()
